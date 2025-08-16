0aa893aadaed6a4b69765ca83ea52eaf140ed2c7
heisenberg-611 <lnsaha1214@gmail.com>
2025-08-16 14:50:20 +0600

    Implemented `levelSum` in Task 6 and `subtractSummation` in Task 5 for Lab 6.

--- CSE220-Summer25/Lab6/src/Task5.java
+++ CSE220-Summer25/Lab6/src/Task5.java
@@ -6,12 +6,25 @@ public class Task5 {
     // This method takes only 1 parameter which is root of the given tree
     // This method returns an Integer
     // At times you may need to typeCast root.elem to Integer
-    // You can use extra helper private static methods with extra extra params as per need
+    // You can use extra helper private static methods with extra extra params as per a need
     public static Integer subtractSummation( BTNode root ){
         //TO DO
+        int sum = 0;
+        Integer sum_L = sumL ( root , 0, sum);
+        Integer sum_R = sumR ( root , 0, sum);
+        return sum_L-sum_R + ( Integer ) root.elem;
+    }
 
-        return null; //remove this line
+    private static int sumL ( BTNode root, int lvl, int sum){
+        if(root == null) return sum;
+        sum += (Integer)root.elem;
+        return sumL ( root.left , lvl+1, sum);
+    }
+
+    private static int sumR (BTNode root , int lvl, int sum){
+        if(root == null) return sum;
+        sum += (Integer)root.elem;
+        return sumR ( root.right , lvl+1, sum);
     }
     //============================================================================
-
 }
--- CSE220-Summer25/Lab6/src/Task6.java
+++ CSE220-Summer25/Lab6/src/Task6.java
@@ -9,7 +9,28 @@ public class Task6 {
     // You can use extra helper private static methods with extra extra params as per need
     public static Integer levelSum( BTNode root ){
         //TO DO
-        return null; //remove this line
+        int sum = 0;
+        Integer sum_O = SumO ( root , 0);
+        Integer sum_E = SumE ( root , 0);
+        return (sum_O - sum_E);
+    }
+
+    private static int SumO(BTNode root, int lvl){
+        if(root == null) return 0;
+        int sum = 0;
+        if(lvl%2==1){
+            sum += (Integer) root.elem;
+        }
+        return (sum+SumO(root.left, lvl+1) + SumO(root.right, lvl+1));
+    }
+
+    private static int SumE(BTNode root, int lvl){
+        if(root == null) return 0;
+        int sum = 0;
+        if(lvl%2==0){
+            sum += (Integer) root.elem;
+        }
+        return sum+SumE(root.left, lvl+1) + SumE(root.right, lvl+1);
     }
     //============================================================================
 
